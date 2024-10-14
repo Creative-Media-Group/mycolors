e5ced63321adb334157f207e866d1cf294508aee
tct123 <42028373+tct123@users.noreply.github.com>
2024-10-14 18:44:43 +0200

    Update __init__.py

---
+++ mycolors/__init__.py
@@ -0,0 +1,9 @@
+def color(color: str):
+    color = color.upper()
+    if color == "RED":
+        colours = []
+        return colours
+    #
+    if color == "RED":
+        colours = ["#c4e6b6",]
+        return colours
